9ff2976dd2969da0c17ec0f53f3cc1ea14c4b9e6
Jesse Riddle <jesser@hranswerlink.com>
2012-08-20 22:00:48 -0400

    starting socket, pms5005 compiles, should be mostly complete.

---
+++ src/main/c/sockets/X80ProUdpSocket.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef X80PROUDPSOCKET_H
+#define X80PROUDPSOCKET_H
+
+typedef struct X80ProUdpSocket_t
+{
+    char* robotIp;
+    int robotPort;
+    
+    
+} X80ProUdpSocket;
+
+#endif
