9ae601ea53c1bc1c33c7dfbda29402eec3cff673
Jesse Riddle <jesser@hranswerlink.com>
2013-02-21 23:46:53 -0500

    Starting PMB5010 support

--- src/main/java/edu/ius/robotics/robots/boards/PMB5010.java
+++ src/main/java/edu/ius/robotics/robots/boards/PMB5010.java
@@ -1,7 +1,7 @@
 package edu.ius.robotics.robots.boards;
 
 /* The following is taken pretty much directly from the 
- * PMB5010 serial protocol documentation 
+ * PMB5010 serial protocol documentation
  */
 
 public class PMB5010
@@ -29,60 +29,237 @@ public class PMB5010
 		
 		public static class ADPCMState 
 		{
-			short valprev; /* previous output value */
-			char index; /* index into step size table */
-			
-			public short getValPrev()
-			{
-				return this.valprev;
-			}
-			
-			public void setValPrev(int valprev)
-			{
-				this.valprev = (short)valprev;
-			}
-			
-			public char getIndex()
-			{
-				return this.index;
-			}
-			
-			public void setIndex(int index)
-			{
-				this.index = (char)index;
-			}
-		};
+			/* ignoring the convention to wrap these with getters and setters */
+			public short previousOutput; /* previous output value */
+			public char index; /* index into step size table */
+		}
 		
 		ADPCMState adpcmState;
 		
 		public ADPCM()
 		{
-			adpcmState.setValPrev(0);
-			adpcmState.setIndex(0);
+			adpcmState.previousOutput = 0;
+			adpcmState.index = 0;
 		}
 		
 		// lpInData points to msg[6], nLen = Data_Len
-		public short[] dealWithAudio(char[] inData)
+		public short[] dealWithAudio(byte[] input)
 		{
-			return adpcmDecode(inData);
+			return decodeADPCM(input);
 		}
 		
-		public short[] adpcmDecode(char[] inData)
+		public short[] decodeADPCM(byte[] input)
 		{
-			short[] outData = new short[10];
+			int outputLength = 2*input.length;
+			short[] output = new short[outputLength];
 			
+			short predictedOutput = adpcmState.previousOutput;
+			char index = adpcmState.index;
+			int step = stepsizeTable[index];
+			
+			short predictedOutputDelta;
 			int sign;
 			int delta;
-			int step;
-			int valPred;
-			int vpDiff;
-			int index;
-			int inputBuffer;
-			int bufferStep;
 			
+			boolean bufferStep = false;
+			int inputIndex = 0;
+			int outputIndex = 0;
 			
+			for (int i = output.length; 0 < i; i--)
+			{
+				/* Step 1: Get the data value */
+				if (bufferStep)
+				{
+					delta = (byte) (input[inputIndex] & 0xff);
+				}
+				else
+				{
+					delta = (byte) (input[++inputIndex] >>> 4 & 0xff);
+				}
+				bufferStep = !bufferStep;
+				
+				/* Step 2: Find new index value (for later) */
+				index += indexTable[delta];
+				if (index < 0)
+				{
+					index = 0;
+				}
+				else if (88 < index)
+				{
+					index = 88;
+				}
+				
+				/* Step 3: Separate sign and magnitude */
+				sign = delta & 0x08;
+				delta = delta & 0x07;
+				
+				/* Step 4: Combine difference and new predicted value */
+				/*
+				 * Computes vpDiff = (delta + 0.5)*step/4, but see comment
+				 * in adpcmCoder();
+				 */
+				
+				predictedOutputDelta = (short) (step >>> 3);
+				if (0 < (delta & 0x04))
+				{
+					predictedOutputDelta += step;
+				}
+				
+				if (0 < (delta & 0x02))
+				{
+					predictedOutputDelta += step >> 1;
+				}
+				
+				if (0 < (delta & 0x01))
+				{
+					predictedOutputDelta += step >> 2;
+				}
+				
+				if (0 < sign)
+				{
+					predictedOutput -= predictedOutputDelta;
+				}
+				else
+				{
+					predictedOutput += predictedOutputDelta;
+				}
+				
+				/* Step 5: Clamp output value */
+				if (32767 < predictedOutput)
+				{
+					predictedOutput = 32767;
+				}
+				else if (predictedOutput < -32768)
+				{
+					predictedOutput = -32768;
+				}
+				
+				/* Step 6: Update step value */
+				step = stepsizeTable[index];
+				
+				/* Step 7: Output value */
+				output[outputIndex++] = predictedOutput;
+			}
 			
-			return outData;
+			adpcmState.previousOutput = predictedOutput;
+			adpcmState.index = index;
+			
+			return output;
+		}
+		
+		public byte[] encodeADPCM(short[] input)
+		{
+			int outputLength = 2*input.length;
+			byte[] output = new byte[outputLength];
+			short predictedOutput = adpcmState.previousOutput;
+			char index = adpcmState.index;
+			int step = stepsizeTable[index];
+			
+			int outputHolder = 0;
+			boolean bufferStep = true;
+			int inputIndex = 0;
+			int outputIndex = 0;
+			for (int i = 2*input.length; 0 < i; --i)
+			{
+				short inputValue = input[i++];
+				
+				/* Step 1: Compute difference with previous value */
+				int diff = inputValue - predictedOutput;
+				int sign = (diff < 0) ? 8 : 0;
+				if (0 < sign)
+				{
+					diff = -diff;
+				}
+				
+				/* Step 2: Divide and clamp */
+				/* Note
+				 * This code *approximately* computes:
+				 * 	delta = diff*4/step;
+				 * 	predictedOutputDelta = (0.5*delta)*step/4
+				 * but in shift step bits are dropped. The net results of this is
+				 * that even if you have fast mul/div hardware you cannot put it to
+				 * good use since the fixup would be too expensive.
+				 */
+				int delta = 0;
+				int predictedOutputDelta = step >>> 3;
+				
+				if (step <= diff)
+				{
+					delta = 4;
+					diff -= step;
+					predictedOutputDelta += step;
+				}
+				
+				step >>= 1;
+				if (step <= diff)
+				{
+					delta |= 2;
+					diff -= step;
+					predictedOutputDelta += step;
+				}
+				
+				step >>= 1;
+				if (step <= diff)
+				{
+					delta |= 1;
+					predictedOutputDelta += step;
+				}
+				
+				/* Step 3: Update previous value */
+				if (0 < sign)
+				{
+					predictedOutput -= predictedOutputDelta;
+				}
+				else
+				{
+					predictedOutput += predictedOutputDelta;
+				}
+				
+				/* Step 4: Clamp previous value to 16 bits */
+				if (32767 < predictedOutput)
+				{
+					predictedOutput = 32767;
+				}
+				else if (predictedOutput < -32768)
+				{
+					predictedOutput = -32768;
+				}
+				
+				/* Step 5: Assemble value, update index and step values */
+				delta |= sign;
+				
+				index += indexTable[delta];
+				if (index < 0)
+				{
+					index = 0;
+				}
+				else if (88 < index)
+				{
+					index = 88;
+				}
+				
+				/* Step 6: Output value */
+				if (bufferStep)
+				{
+					outputHolder = (delta << 4) & 0xf0;
+				}
+				else
+				{
+					output[outputIndex++] = (byte) (((delta & 0x0f) | outputHolder) & 0xff);
+				}
+				bufferStep = !bufferStep;
+			}
+			
+			/* Output last step, if needed */
+			if (!bufferStep)
+			{
+				output[outputIndex++] = (byte) outputHolder;
+			}
+			
+			adpcmState.previousOutput = predictedOutput;
+			adpcmState.index = index;
+			
+			return output;
 		}
 	}
 	
